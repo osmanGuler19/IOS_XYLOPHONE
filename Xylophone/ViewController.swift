ea0d0e7c723b7168a27c57e8061527e08dbb6c02
osmanGuler19 <osman-guler@outlook.com>
2023-11-07 23:10:45 +0300

    finished project

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , AVAudioPlayerDelegate {
+    
+    var player : AVAudioPlayer!
+    var soundArray = ["C", "D", "E", "F", "G", "A", "B"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func playNote(_ sender: UIButton) {
+        var selectedSound = soundArray[sender.tag-1]
+        playingSound(chooseSound: selectedSound)
+    }
+    
+    func playingSound(chooseSound: String){
+        let soundUrl = Bundle.main.url(forResource: chooseSound, withExtension: ".wav", subdirectory: "Sounds")
+        do{
+            try player = AVAudioPlayer(contentsOf: soundUrl!)
+            player.prepareToPlay()
+            player.play()
+        }catch{
+            print(error)
+        }
+    }
 }
 
